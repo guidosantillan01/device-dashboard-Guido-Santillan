0c7cada2eac2f70704e85a10a7b7c629b6cbcb2c
Guido Santillan <guidosantillan01@gmail.com>
2019-08-18 22:13:46 +0200

    Solve regeneratorRuntime is not defined problem by installing and adding to webpack 'babel-polyfill'

--- webpack.config.js
+++ webpack.config.js
@@ -4,34 +4,34 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
-  entry: {
-    app: ['react-hot-loader/patch', './src/index.js'],
-  },
-  output: {
-    publicPath: '/',
-    filename: 'js/[name].js',
-  },
-  plugins: [
-    new webpack.HotModuleReplacementPlugin(),
-    new HtmlWebpackPlugin({
-      template: './index.html',
-      inject: 'body',
-    }),
-    new MiniCssExtractPlugin(),
-  ],
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader',
-        },
-      },
-      {
-        test: /\.css$/,
-        use: [MiniCssExtractPlugin.loader, 'css-loader'],
-      },
+    entry: {
+        app: ['react-hot-loader/patch', 'babel-polyfill', './src/index.js'],
+    },
+    output: {
+        publicPath: '/',
+        filename: 'js/[name].js',
+    },
+    plugins: [
+        new webpack.HotModuleReplacementPlugin(),
+        new HtmlWebpackPlugin({
+            template: './index.html',
+            inject: 'body',
+        }),
+        new MiniCssExtractPlugin(),
     ],
-  },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                },
+            },
+            {
+                test: /\.css$/,
+                use: [MiniCssExtractPlugin.loader, 'css-loader'],
+            },
+        ],
+    },
 };
