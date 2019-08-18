1e8ac3b2364242e1f6145f47de2d655f9a0da132
Guido Santillan <guidosantillan01@gmail.com>
2019-08-18 19:09:34 +0200

    Implement button handler to change the status of the reading.

--- src/DeviceDataItem.js
+++ src/DeviceDataItem.js
@@ -1,16 +1,35 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-const DeviceDataItem = ({ reading }) => {
-    const { active, name, timestamp, unit, value } = reading;
-    return (
-        <div className="device-data-item">
-            <p>{active ? 'Active' : 'Inactive'}</p>
-            <p>Name: {name}</p>
-            <p>Timestamp: {timestamp}</p>
-            <p>Unit: {unit}</p>
-            <p>Value: {value}</p>
-        </div>
-    );
-};
+class DeviceDataItem extends Component {
+    async handleButtonClick(readingName, readingStatus) {
+        try {
+            const newStatus = !readingStatus;
+            const res = await fetch(
+                `http://127.0.0.1:8888/devices/${readingName}?active=${newStatus}`,
+            );
+            const response = await res.json();
+            console.log(response);
+        } catch (error) {
+            throw new Error(error);
+        }
+    }
+
+    render() {
+        const { active, name, timestamp, unit, value } = this.props.reading;
+
+        return (
+            <div className="device-data-item">
+                <p>{active ? 'Active' : 'Inactive'}</p>
+                <button onClick={() => this.handleButtonClick(name, active)}>
+                    Change status
+                </button>
+                <p>Name: {name}</p>
+                <p>Timestamp: {timestamp}</p>
+                <p>Unit: {unit}</p>
+                <p>Value: {value}</p>
+            </div>
+        );
+    }
+}
 
 export default DeviceDataItem;
