c50f0d45afe8929e48598295f1c66347395331c3
Guido Santillan <guidosantillan01@gmail.com>
2019-08-18 11:49:12 +0200

    Add count of active and inactive readings

--- src/DeviceData.js
+++ src/DeviceData.js
@@ -3,8 +3,28 @@ import React from 'react';
 import DeviceDataItem from './DeviceDataItem';
 
 const DeviceData = ({ data }) => {
+    let activeCounter = 0;
+    let inactiveCounter = 0;
+
+    data.map(reading => {
+        if (reading.active) {
+            activeCounter++;
+        } else {
+            inactiveCounter++;
+        }
+    });
+
     return (
         <div>
+            {activeCounter + inactiveCounter === data.length ? (
+                <div>
+                    <p>Active readings: {activeCounter}</p>
+                    <p>Inactive readings: {inactiveCounter}</p>
+                </div>
+            ) : (
+                'Error counting readings'
+            )}
+
             {data.map(reading => {
                 return <DeviceDataItem reading={reading} key={reading.name} />;
             })}
